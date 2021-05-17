408b52540936f9c6638f177319a9b73904f7ccea
Pete Miller <miller.pete@gmail.com>
2021-05-16 23:13:09 -0700

    Create JS API for Search to set as default search provider

---
+++ components/brave_search/browser/brave_search_default_host_private.cc
@@ -0,0 +1,25 @@
+// Copyright (c) 2021 The Brave Authors. All rights reserved.
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this file,
+// you can obtain one at http://mozilla.org/MPL/2.0/.
+
+#include "brave/components/brave_search/browser/brave_search_default_host_private.h"
+
+#include <string>
+#include <utility>
+
+#include "components/prefs/pref_service.h"
+#include "components/search_engines/template_url_service.h"
+
+namespace brave_search {
+
+BraveSearchDefaultHostPrivate::~BraveSearchDefaultHostPrivate() {}
+
+void BraveSearchDefaultHostPrivate::GetCanSetDefaultSearchProvider(
+    GetCanSetDefaultSearchProviderCallback callback) {
+  std::move(callback).Run(false);
+}
+
+void BraveSearchDefaultHostPrivate::SetIsDefaultSearchProvider() {}
+
+}  // namespace brave_search
