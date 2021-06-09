637977e09a3c9afda8c9a6ec1326182ac3f992dd
mkarolin <max@brave.com>
2021-06-09 09:27:41 -0400

    fixup! [FOLLOWUP] CachedStorageArea c'tor signature changed.

--- chromium_src/third_party/blink/renderer/modules/storage/dom_window_storage.cc
+++ chromium_src/third_party/blink/renderer/modules/storage/dom_window_storage.cc
@@ -228,7 +228,7 @@ StorageArea* BraveDOMWindowStorage::ephemeralLocalStorage() {
   auto storage_area = base::MakeRefCounted<CachedStorageArea>(
       CachedStorageArea::AreaType::kSessionStorage, window->GetSecurityOrigin(),
       controller->TaskRunner(), namespaces->local_storage(),
-      window->GetFrame()->GetDocument()->IsPrerendering());
+      /*is_session_storage_for_prerendering=*/false);
 
   // Ephemeral localStorage never persists stored data, which is also how
   // sessionStorage works. Due to this, when opening up a new ephemeral
